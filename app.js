0b5c663a1ce2b270437bdba61dce43b9a5e03055
ChoiYeoJin <sonata7531@gmail.com>
2023-12-12 20:54:33 +0900

    몽고디비 세팅까지

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// require("dotenv").config({ path: ".env.local" });
+
+const port = process.env.PORT;
+const uri = process.env.MONGO_CONNECTION;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB 연결 성공");
+  })
+  .catch((err) => {
+    console.log("MongoDB 연결 실패 : ", err);
+  });
+
+module.exports = app;
