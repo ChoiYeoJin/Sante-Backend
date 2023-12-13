9dd766cb7899f0581f70d8beac793af7a99439c6
jining <hwangjii1006@gmail.com>
2023-12-13 15:24:04 +0900

    registerRouter 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const userRouter = require("./router/userRouter");
+const registerRouter = require("./router/registerRouter");
 
 const port = process.env.PORT;
 const uri = process.env.MONGO_CONNECTION;
@@ -16,6 +17,7 @@ app.use(cors());
 
 // 라우터 세팅
 app.use("/user", userRouter);
+app.use("/register", registerRouter);
 
 mongoose
   .connect(uri)
@@ -25,5 +27,10 @@ mongoose
   .catch((err) => {
     console.log("MongoDB 연결 실패 : ", err);
   });
+
+
+app.listen(process.env.port, () => {
+  console.log(`server on port ${process.env.port}`);
+});
 
 module.exports = app;
