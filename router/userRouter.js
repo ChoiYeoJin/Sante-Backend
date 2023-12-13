03a2e730af5d71b153c98a30fc67d24b0304b3ac
[SW7] 박소영 <soyo19585@gmail.com>
2023-12-13 20:24:53 +0900

    Func: update user

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,4 +1,33 @@
 const router = require("express").Router();
-const User = require("../models/users");
+const { User } = require("../models/users");
+
+router.put("/", async (req, res) => {
+  try {
+    const user = req.body;
+
+    console.log("user", user);
+
+    const updatedUser = await User.findOneAndUpdate(
+      {
+        email: user.email,
+      },
+      user,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    res
+      .status(200)
+      .json({ message: "사용자 정보가 업데이트되었습니다.", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "서버 오류: 사용자 정보를 업데이트할 수 없습니다.",
+    });
+  }
+});
 
 module.exports = router;
