b44a3c53aca08e5e8330cd7e50f6c3fd1ee0cce8
[SW7] 박소영 <soyo19585@gmail.com>
2023-12-13 20:49:59 +0900

    fix merge conflict

--- router/userRouter.js
+++ router/userRouter.js
@@ -17,7 +17,7 @@ router.get("/:email", async (req, res) => {
     console.log("Received request for email:", userEmail);
 
     // 사용자 이메일 주소를 기반으로 데이터를 조회해서 user로 저장
-    const user = await User.findOne({email: userEmail}); // 걍 이메일
+    const user = await User.findOne({ email: userEmail }); // 걍 이메일
     console.log("User found:", user);
 
     if (!user) {
@@ -42,6 +42,35 @@ router.get("/:email", async (req, res) => {
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const user = req.body;
+
+    console.log("user", user);
+
+    const updatedUser = await User.findOneAndUpdate(
+      {
+        email: user.email,
+      },
+      user,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    res
+      .status(200)
+      .json({ message: "사용자 정보가 업데이트되었습니다.", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "서버 오류: 사용자 정보를 업데이트할 수 없습니다.",
+    });
   }
 });
 
