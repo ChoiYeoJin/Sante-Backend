905ca5b35faab430cf47fc4059d63bd8cc3d2383
ChoiYeoJin <sonata7531@gmail.com>
2023-12-20 11:27:43 +0900

    리미트 세팅

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const uri = process.env.MONGO_CONNECTION;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
 // 라우터 세팅
