5bda8988d1095d121ead411055c0ca14d3d67581
ChoiYeoJin <sonata7531@gmail.com>
2024-01-12 18:55:38 +0900

    bcyript 적용

--- router/registerRouter.js
+++ router/registerRouter.js
@@ -1,16 +1,23 @@
 const router = require("express").Router();
 const { User } = require("../models/users");
+const bcrypt = require("bcrypt");
+
+async function hashPassword(password) {
+  const saltRounds = 10; // 솔트 라운드 수, 더 높을수록 해시는 더 강력하지만 느려집니다.
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
 
 router.post("", async (req, res) => {
   try {
     const { email, password, gender, age, todayCalory } = req.body;
-
+    const hashedPassword = await hashPassword(password);
     const newUser = new User({
-      email,
-      password,
-      gender,
-      age,
-      todayCalory,
+      email: email,
+      password: hashedPassword,
+      gender: gender,
+      age: age,
+      todayCalory: todayCalory,
     });
 
     await newUser.save();
