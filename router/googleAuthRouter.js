a6ccc585f2ee5ce46447c78d8332b92be4a2b11e
[SW7] 박소영 <soyo19585@gmail.com>
2024-01-15 13:14:36 +0900

    feat: 구글 로그인(미완)

--- router/googleAuthRouter.js
+++ router/googleAuthRouter.js
@@ -2,68 +2,157 @@ require("dotenv").config();
 const { OAuth2Client } = require("google-auth-library");
 const router = require("express").Router();
 const axios = require("axios");
+const { User } = require("../models/users");
+const createUserToken = require("../utils/jwtUtil");
 
-const redirectUrl = "http://kdt-sw-7-team04.elicecoding.com/api/googleOauth";
+const oAuth2Client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_PWD,
+  process.env.GOOGLE_REDIRECT_URL
+);
 
-//NOTE: 리디렉션 url 생성
-router.post("/", async function (req, res, next) {
+const calculateAge = (birthYear) => {
+  // 현재 년도 가져오기
+  const currentYear = new Date().getFullYear();
+  // 나이 계산
+  const age = currentYear - birthYear;
+
+  let ageNumber;
+
+  switch (true) {
+    case age >= 12 && age <= 14:
+      ageNumber = 1;
+      break;
+    case age >= 15 && age <= 18:
+      ageNumber = 2;
+      break;
+    case age >= 19 && age <= 29:
+      ageNumber = 3;
+      break;
+    case age >= 30 && age <= 49:
+      ageNumber = 4;
+      break;
+    case age >= 50 && age <= 64:
+      ageNumber = 5;
+      break;
+    default:
+      ageNumber = 6;
+      break;
+  }
+  return ageNumber;
+};
+
+router.post("/login", async function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Referrer-Policy", "no-referrer-when-downgrade"); //NOTE: for using http
 
-  const oAuth2Client = new OAuth2Client(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    redirectUrl
-  );
-
   const authorizeUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline", //NOTE force refresh token to be created
-    //액세스 토큰은 짧은 유효 시간을 가지고 있어, 만료되면 사용자가 다시 로그인해야 한다.
-    //하지만 offline 접근 유형을 설정하면, 애플리케이션은 리프레시 토큰을 받게 되고,
-    //사용자가 매번 로그인하지 않아도, 애플리케이션이 사용자의 데이터에 지속적으로 접근할 수 있게 된다.
-    scope: "https://www.googleapis.com/auth/userinfo.profile openid",
-    prompt: "consent", //NOTE sign-in한 상태여도 체크
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/user.birthday.read",
+      "https://www.googleapis.com/auth/user.gender.read",
+    ],
   });
 
   res.json({ url: authorizeUrl });
 });
 
-//
 async function getUserData(access_token) {
-  //   const response = await fetch(
-  //     `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
-  //   );
-  //   const data = await response.json();
-  //   console.log("data", data);
   try {
-    const response = await axios.get(
-      `https://www.googleapis.com/oauth2/v3/userinfo`,
-      {
-        params: { access_token: access_token },
-      }
+    const [basicProfileResponse, detailedProfileResponse] = await Promise.all([
+      axios.get(`https://www.googleapis.com/oauth2/v3/userinfo`, {
+        headers: { Authorization: `Bearer ${access_token}` },
+      }),
+      axios.get(
+        `https://people.googleapis.com/v1/people/me?personFields=birthdays,genders,emailAddresses`,
+        {
+          headers: { Authorization: `Bearer ${access_token}` },
+        }
+      ),
+    ]);
+
+    const email = detailedProfileResponse.data.emailAddresses?.[0]?.value;
+    // console.log("email:", email);
+
+    const gender = detailedProfileResponse.data.genders?.[0]?.value;
+    // console.log("Gender:", gender);
+
+    const birthdateData = detailedProfileResponse.data.birthdays?.find(
+      (birthday) => birthday.metadata.primary
     );
-    console.log("data", response.data);
-    return response.data;
+
+    let birthdayYear = "";
+    if (birthdateData) {
+      const { year } = birthdateData.date;
+      console.log(`Birthday: ${year}`);
+      birthdayYear = String(year);
+    } else {
+      console.log("Primary birthday is not available.");
+    }
+
+    const age = calculateAge(birthdayYear);
+
+    return {
+      basicProfile: basicProfileResponse.data,
+      detailedProfile: {
+        email,
+        password: email,
+        age,
+        gender,
+      },
+    };
   } catch (error) {
     console.error("Error fetching user data", error);
     throw error;
   }
 }
 
-router.get("/", async function (req, res, next) {
-  const code = req.query.code;
+router.post("/login/token", async function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Referrer-Policy", "no-referrer-when-downgrade"); //NOTE: for using http
+
+  const code = req.body.code;
+  console.log("my code", code);
+
   try {
-    const oAuth2Client = new OAuth2Client(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      redirectUrl
-    );
     const response = await oAuth2Client.getToken(code);
     await oAuth2Client.setCredentials(response.tokens);
     console.log("Tokens acquired");
     const user = oAuth2Client.credentials;
-    console.log("credentials", user);
-    await getUserData(user.access_token);
+    const userDetails = await getUserData(user.access_token);
+    const genderInfo = userDetails.detailedProfile.gender;
+    const whatGender = (genderInfo) => {
+      if (genderInfo === "women") {
+        return "여자";
+      } else {
+        return "남자";
+      }
+    };
+    const gender = whatGender(genderInfo);
+    const age = userDetails.detailedProfile.age;
+    const email = userDetails.basicProfile.email;
+    const password = userDetails.basicProfile.password;
+
+    const existingUser = await User.findOne({ email: email });
+    console.log("-----existingUser------", existingUser);
+
+    if (!existingUser) {
+      console.log("email", email);
+      console.log("password", password);
+      console.log("gender", gender);
+      console.log("age", age);
+      const newUser = new User({
+        email,
+        password,
+        gender,
+        age,
+      });
+      await newUser.save();
+    }
+    const jwToken = createUserToken(email);
+    return res.json({ jwToken });
   } catch (err) {
     console.error("Error with signing in with Google", err);
   }
